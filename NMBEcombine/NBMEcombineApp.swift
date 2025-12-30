c02537af2cf35cf8fc84b02cb287df1d9e60d104
zone6draco <7nwsz75xgy@privaterelay.appleid.com>
2025-12-30 01:09:24 -0500

    Add files via upload
    Fixed naming

---
+++ NMBEcombine/NBMEcombineApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct NBMEcombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }.defaultSize(width: 600, height: 740)
+    }
+}
---
+++ NMBEcombine/Utilities/AppPaths.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+struct AppPaths {
+    static let appSupport: URL = {
+        let base = FileManager.default.urls(
+            for: .applicationSupportDirectory,
+            in: .userDomainMask
+        )[0]
+
+        let dir = base.appendingPathComponent("NBMEcombine", isDirectory: true)
+        try? FileManager.default.createDirectory(
+            at: dir,
+            withIntermediateDirectories: true
+        )
+        return dir
+    }()
+
+    static var downloads: URL {
+        appSupport.appendingPathComponent("downloads", isDirectory: true)
+    }
+
+    static var processed: URL {
+        appSupport.appendingPathComponent("processed", isDirectory: true)
+    }
+}
